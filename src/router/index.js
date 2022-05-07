affd56724ade388b610d6c063ee040d5629292c0
pidokige02 <pidokige0204@gmail.com>
2022-05-07 12:07:13 +0900

    initial commit for event_manager

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import EventCreate from '../views/EventCreate.vue'
+import EventList from '../views/EventList.vue'
+import EventShow from '../views/EventShow.vue'
+
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'event-list',
+    component: EventList
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate
+  },
+  {
+    path: '/event/:id',
+    name: 'event-show',
+    component: EventShow,
+    props: true
   }
 ]
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,26 @@
 import { createStore } from 'vuex'
 
+import * as user from '@/store/modules/user.js'
+import * as event from '@/store/modules/event.js'
+
 export default createStore({
   state: {
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ]
   },
   mutations: {
   },
   actions: {
   },
   modules: {
+    user,
+    event
   }
 })
